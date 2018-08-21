7643669b7d8b0ccce9e96f5da3301e31eddf000a
Yakov <crazyk@bk.ru>
2018-08-22 00:25:05 +0200

    hamburger-menu + fixes

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//hamburger menu 
+
+let menu = document.querySelector('#hamburgerMenu');
+let openButton = document.querySelector('#menuOpen');
+let closeButton = document.querySelector('#menuClose');
+let body = document.querySelector('body');
+let menuOption = document.querySelectorAll('.hamburger-menu__link')
+
+openButton.addEventListener('click', function(){
+    menu.classList.toggle('hamburger-menu__active');
+    body.classList.toggle('body-active-menu');
+})
+
+closeButton.addEventListener('click', function(){
+    menu.classList.toggle('hamburger-menu__active');
+    body.classList.toggle('body-active-menu');
+})
+
+for (var i = 0; i < menuOption.length; i++) {
+    menuOption[i].addEventListener('click', function(){
+        menu.classList.toggle('hamburger-menu__active');
+        body.classList.toggle('body-active-menu');
+    })
+}
+
+//
+
+
