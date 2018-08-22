1c32b7f21b758a139233517f85da350d12f92d83
Yakov <crazyk@bk.ru>
2018-08-23 00:34:26 +0200

    week4 40% ready

--- js/main.js
+++ js/main.js
@@ -26,6 +26,58 @@ for (var i = 0; i < menuOption.length; i++) {
     })
 }
 
+//widgets
+
+//team & menu accordeons 
+let teamAcco = document.querySelector('.team-accordeon');
+let menuAcco = document.querySelector('.menu-accordeon');
+let phonesClose = document.querySelector('.phones-close');
+let menuContent = document.querySelector('.menu-accordeon__content');
+let menuItem = document.querySelector('.menu-accordeon__item');
+
+teamAcco.addEventListener('click', function(e){
+    e.preventDefault();
+    if (e.target.nodeName === 'A'){
+        e.target.parentNode.classList.toggle('team-active')
+    }
+})
+
+menuAcco.addEventListener('click', function(e){
+    e.preventDefault();
+    
+    if (e.target.nodeName === 'A'){
+        e.target.parentNode.classList.toggle('menu-active')
+    }
+
+    if(e.target.nodeName === 'DIV'){
+        e.target.parentNode.parentNode.classList.toggle('menu-active')
+    }
+
+    if(e.target.nodeName === 'IMG'){
+        e.target.parentNode.parentNode.parentNode.classList.toggle('menu-active')
+    }
+
+})
+
+
+//slider
+//slider rotation
 
 
 
+//ingredients popup slider__ingredients-active
+let ingredients = document.querySelector('.slider__ingredients');
+let ingredientsClose = document.querySelector('.ingredients-close');
+
+ingredients.addEventListener('mouseover', function(e){
+   ingredients.classList.add('slider__ingredients-active')
+})
+
+ingredients.addEventListener('mouseleave', function(e){
+    ingredients.classList.remove('slider__ingredients-active')
+ })
+
+ ingredientsClose.addEventListener('click', function(e){
+    e.preventDefault();
+    ingredients.classList.remove('slider__ingredients-active')
+ })
